1c53ba843936acaff9e4bb514707e3ef3a156dfd
Gregg Bolinger <gregg@genrocket.com>
2014-03-29 11:59:13 -0500

    added better route handling

--- app/router.js
+++ app/router.js
@@ -1,9 +1,9 @@
 App.Router.map(function() {
 	this.resource('songs' , function() {
-
+		this.route('new');
 	});
 	this.resource('about');
 	this.resource('services', function() {
-		this.resource('service', {path: ':service_id'});
+		this.resource('service', {path: '/show/:service_id'});
 	});
 });
